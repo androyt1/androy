a2f2644cd8b1d18409fc6183e189453e35652245
Andrew Aghoghovwia <androyt1@gmail.com>
2024-03-09 12:45:19 +0000

    commit

---
+++ app/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <section id='contact' className='min-h-screen w-full  px-10 md:px-[100px] mt-60'>
+            <div>
+                <h1 className='text-orange-700 font-bold text-6xl uppercase pt-10'>Contact</h1>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
